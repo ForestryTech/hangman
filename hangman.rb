afd3dafe464089cf190d76400898ee9c4edef455
ForestryTech <jpeterson1973@gmail.com>
2016-11-23 20:19:26 -0800

    First ideas for hangman

---
+++ hangman.rb
@@ -0,0 +1,27 @@
+require "json"
+
+class Game
+    # Chose random Word
+    # Ask Player for Guess
+    # Check player Guess
+    # Display guessed Letters
+    # Display word with correctly guessed Letters
+    # See if there are any saved GameState
+    # Load game if player wants to continue game
+    # Update GameState
+    # Save GameState
+end
+
+class GameState
+    # Word
+    # Letters chosen by Player
+    # Player object
+    # Guesses made
+    # Guesses left
+end
+
+class Player
+    # Get name
+    # Get guess
+    # Save guess to array
+end
